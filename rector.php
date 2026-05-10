f0516e54cabc25abec4cce90bc9e1edd8412b2c5
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2026-05-10 14:14:25 +0300

    chore: scope rector to top-level spec files via glob

--- rector.php
+++ rector.php
@@ -6,10 +6,10 @@ use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
-        __DIR__ . '/lib',
-        __DIR__ . '/test',
-    ]);
+    $rectorConfig->paths(array_merge(
+        [__DIR__ . '/lib', __DIR__ . '/test'],
+        glob(__DIR__ . '/spec/*.php') ?: [],
+    ));
 
     $rectorConfig->sets([
         LevelSetList::UP_TO_PHP_80,
